f799889ea9a165350641cfa8a6174b54179002f5
HasChad <troll.yahya@gmail.com>
2023-10-24 20:13:48 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,97 @@
-/// Application.
-pub mod app;
-
-/// Terminal events handler.
-pub mod event;
-
-/// Widget renderer.
-pub mod ui;
-
-/// Terminal user interface.
-pub mod tui;
-
-/// Application updater.
-pub mod update;
-
-use app::App;
-use event::{Event, EventHandler};
-use ratatui::{backend::CrosstermBackend, Terminal};
-use tui::Tui;
-use update::update;
-
 use color_eyre::eyre::Result;
+use crossterm::{
+    event::{self, Event::Key, KeyCode::Char},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Terminal},
+    widgets::Paragraph,
+};
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    // Create an application.
-    let mut app = App::new();
+pub type Frame<'a> = ratatui::Frame<'a, CrosstermBackend<std::io::Stderr>>;
 
-    // Initialize the terminal user interface.
-    let backend = CrosstermBackend::new(std::io::stderr());
-    let terminal = Terminal::new(backend)?;
-    let events = EventHandler::new(250);
-    let mut tui = Tui::new(terminal, events);
-    tui.enter()?;
+fn startup() -> Result<()> {
+    enable_raw_mode()?;
+    execute!(std::io::stderr(), EnterAlternateScreen)?;
+    Ok(())
+}
 
-    // Start the main loop.
-    while !app.should_quit {
-        // Render the user interface.
-        tui.draw(&mut app)?;
-        // Handle events.
-        match tui.events.next()? {
-            Event::Tick => {}
-            Event::Key(key_event) => update(&mut app, key_event),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-        };
+fn shutdown() -> Result<()> {
+    execute!(std::io::stderr(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+// App state
+struct App {
+    counter: i64,
+    should_quit: bool,
+}
+
+// App ui render function
+fn ui(app: &App, f: &mut Frame<'_>) {
+    f.render_widget(
+        Paragraph::new(format!("Counter: {}", app.counter)),
+        f.size(),
+    );
+}
+
+// App update function
+fn update(app: &mut App) -> Result<()> {
+    if event::poll(std::time::Duration::from_millis(250))? {
+        if let Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                match key.code {
+                    Char('j') => app.counter += 1,
+                    Char('k') => app.counter -= 1,
+                    Char('q') => app.should_quit = true,
+                    _ => {}
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    // ratatui terminal
+    let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+
+    // application state
+    let mut app = App {
+        counter: 0,
+        should_quit: false,
+    };
+
+    loop {
+        // application update
+        update(&mut app)?;
+
+        // application render
+        t.draw(|f| {
+            ui(&app, f);
+        })?;
+
+        // application exit
+        if app.should_quit {
+            break;
+        }
     }
 
-    // Exit the user interface.
-    tui.exit()?;
+    Ok(())
+}
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    startup()?;
+
+    let result = run();
+
+    // teardown terminal before unwrapping Result of app run
+    shutdown()?;
+
+    result?;
+
     Ok(())
 }
